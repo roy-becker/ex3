5c5a57dbf25fffd1c634f3251785a345ad816f25
roy-becker <103379587+roy-becker@users.noreply.github.com>
2022-06-07 21:34:22 +0300

    Add files via upload

---
+++ HealthPoints.cpp
@@ -0,0 +1,164 @@
+#include "HealthPoints.h"
+
+
+HealthPoints::HealthPoints()
+{
+    m_health = DEFAULT_HEALTH;
+    m_maxHealth = DEFAULT_HEALTH;
+}
+
+HealthPoints::HealthPoints(int n)
+{
+    if (n > 0)
+    {
+        m_health = n;
+        m_maxHealth = n;
+    }
+    else
+    {
+        throw HealthPoints::InvalidArgument;
+    }
+}
+
+HealthPoints& HealthPoints::operator+(const HealthPoints& n)
+{
+    HealthPoints result = new HealthPoints(this);
+    result += n;
+    return result;
+}
+
+HealthPoints& HealthPoints::operator+(const int x, const HealthPoints& y)
+{
+    return y + x;
+}
+
+HealthPoints& HealthPoints::operator-(const int n)
+{
+    HealthPoints result = new HealthPoints(this);
+    result -= n;
+    return result;
+}
+
+void HealthPoints::operator+=(const int n)
+{
+    this->m_health += n;
+    if (this->m_health < 0)
+    {
+        this->m_health = 0;
+    }
+    else if (this->m_health > this->m_maxHealth)
+    {
+        this->m_health = this->m_maxHealth;
+    }
+    return this;
+}
+
+void HealthPoints::operator-=(const int n)
+{
+    this->m_health -= n;
+    if (this->m_health < 0)
+    {
+        this->m_health = 0;
+    }
+    else if (this->m_health > this->m_maxHealth)
+    {
+        this->m_health = this->m_maxHealth;
+    }
+    return this;
+}
+
+bool HealthPoints::operator==(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator==(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator==(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+bool HealthPoints::operator!=(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator!=(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator!=(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+bool HealthPoints::operator<=(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator<=(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator<=(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+bool HealthPoints::operator>=(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator>=(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator>=(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+bool HealthPoints::operator<(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator<(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator<(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+bool HealthPoints::operator>(const HealthPoints& other)
+{
+    return this->m_health == other.m_health;
+}
+
+bool HealthPoints::operator>(const int n)
+{
+    return this->m_health == n;
+}
+
+bool HealthPoints::operator>(const int x, const HealthPoints& y)
+{
+    return x == y.m_health;
+}
+
+std::ostream& HealthPoints::operator<<(std::ostream& os, const HealthPoints& healthPoints)
+{
+    os << healthPoints.m_health << "(" << healthPoints.m_maxHealth << ")";
+    return os;
+}
---
+++ HealthPoints.h
@@ -0,0 +1,62 @@
+#ifndef EX3_HealthPoints_H
+#define EX3_HealthPoints_H
+
+
+#include <iostream>
+
+
+#define DEFAULT_HEALTH 100
+
+
+class HealthPoints
+{
+private:
+    int m_health;
+    int m_maxHealth;
+
+public:
+    HealthPoints();
+    HealthPoints(const int n);
+
+    HealthPoints(const HealthPoints&) = default;
+    ~HealthPoints() = default;
+    HealthPoints& HealthPoints(const HealthPoints& other) = default;
+
+    HealthPoints& operator+(const HealthPoints& n);
+    friend HealthPoints& operator+(const int x, const HealthPoints& y);
+    
+    HealthPoints& operator-(const int n);
+    
+    void operator+=(const int n);
+    void operator-=(const int n);
+    
+    bool operator==(const HealthPoints& other);
+    bool operator==(const int n);
+    friend bool operator==(const int x, const HealthPoints& y);
+
+    bool operator!=(const HealthPoints& other);
+    bool operator!=(const int n);
+    friend bool operator!=(const int x, const HealthPoints& y);
+
+    bool operator<=(const HealthPoints& other);
+    bool operator<=(const int n);
+    friend bool operator<=(const int x, const HealthPoints& y);
+
+    bool operator>=(const HealthPoints& other);
+    bool operator>=(const int n);
+    friend bool operator>=(const int x, const HealthPoints& y);
+
+    bool operator<(const HealthPoints& other);
+    bool operator<(const int n);
+    friend bool operator<(const int x, const HealthPoints& y);
+
+    bool operator>(const HealthPoints& other);
+    bool operator>(const int n);
+    friend bool operator>(const int x, const HealthPoints& y);
+
+    friend std::ostream& operator<<(std::ostream& os, const HealthPoints& healthPoints);
+
+    class InvalidArgument {};
+}
+
+#endif //EX3_HealthPoints_H
